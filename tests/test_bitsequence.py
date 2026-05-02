8e2aa6417057b2407069305bfc48a9cc3c9eabb5
allin3x <armin.peklar@gmx.at>
2026-05-02 21:57:46 +0200

    Update test_bitsequence.py

--- tests/test_bitsequence.py
+++ tests/test_bitsequence.py
@@ -1,1 +1,7 @@
-from examples.bitsequence import BitSequence
+from examples.bitsequence.bitsequence import BitSequence
+
+
+class TestBitSequence:
+    def test_bit_sequence(self):
+        bs = BitSequence("1010")
+        assert 8 == 8
